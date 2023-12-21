2ed3417d35b181a79dcdad0b7b6021d7b9786b29
ari-abendstern <ari.abendstern@gmx.com>
2023-12-21 21:45:12 +0000

    enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import express from "express";
+import cors from "cors";
 import { connectToDatabase } from "./services/database.service";
 import { taskRouter } from "./routes/tasks.router";
 
-
 const app = express();
-const port = 8080; 
+const port = 8080;
+
+app.use(cors());
 
 connectToDatabase()
-    .then(() => {
-        app.use("/tasks", taskRouter);
+  .then(() => {
+    app.use("/tasks", taskRouter);
 
-        app.listen(port, () => {
-            console.log(`Server started at http://localhost:${port}`);
-        });
-    })
-    .catch((error: Error) => {
-        console.error("Database connection failed", error);
-        process.exit();
+    app.listen(port, () => {
+      console.log(`Server started at http://localhost:${port}`);
     });
+  })
+  .catch((error: Error) => {
+    console.error("Database connection failed", error);
+    process.exit();
+  });
