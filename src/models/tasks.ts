a3f3345f4b88c0576fbe57a93a865268fa0658f4
ari-abendstern <ari.abendstern@gmx.com>
2023-12-21 21:52:43 +0000

    add more type specificity for list

--- src/models/tasks.ts
+++ src/models/tasks.ts
@@ -1,5 +1,10 @@
 import { ObjectId } from "mongodb";
 
 export default class Tasks {
-  constructor(public title: string, public body: string, public list: string, public id?: ObjectId) {}
+  constructor(
+    public title: string,
+    public body: string,
+    public list: "toDo" | "doing" | "done",
+    public id?: ObjectId
+  ) {}
 }
