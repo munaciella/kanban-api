2553a59f3c6df9b47ed43593cb0aab439379616e
ari-abendstern <ari.abendstern@gmx.com>
2023-12-21 21:36:20 +0000

    add list and id to Tasks

--- src/models/tasks.ts
+++ src/models/tasks.ts
@@ -1,6 +1,5 @@
-import {ObjectId} from "mongodb"
+import { ObjectId } from "mongodb";
 
-export default interface Task {
-    title: string;
-    body: string;
+export default class Tasks {
+  constructor(public title: string, public body: string, public list: string, public id?: ObjectId) {}
 }
