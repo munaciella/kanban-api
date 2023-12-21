dfc7a623423f91dce2b4518bceb5988279f50bbc
ari-abendstern <ari.abendstern@gmx.com>
2023-12-21 21:37:07 +0000

    add database connection

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,4 +1,30 @@
-import * as mongoDB from "mongodb"
-import * as dotenv from "dotenv"
+import * as mongoDB from "mongodb";
+import * as dotenv from "dotenv";
 
-export const collections: {tasks?: mongoDB.Collection} = {}
+dotenv.config();
+export const collections: { tasks?: mongoDB.Collection } = {};
+
+
+export async function connectToDatabase() {
+  try {
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+      process.env.DB_CONN_STRING!
+    );
+
+    await client.connect();
+
+    const db: mongoDB.Db = client.db(process.env.DB_NAME);
+
+    const tasksCollection: mongoDB.Collection = db.collection(
+      process.env.TASKS_COLLECTION_NAME!
+    );
+
+    collections.tasks = tasksCollection;
+
+    console.log(
+      `Successfully connected to database: ${db.databaseName} and collection: ${tasksCollection.collectionName}`
+    );
+  } catch (err) {
+    console.log("error:", err);
+  }
+}
